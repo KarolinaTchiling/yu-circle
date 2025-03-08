c5ada612347a09032faef2f11deeb1c40ee9ec7d
Karolina Tchiling <kdt@my.yorku.ca>
2025-03-08 16:44:57 -0500

    Implemented reset password popup - connected with backend

--- frontend/src/components/Dashboard/SettingComp.tsx
+++ frontend/src/components/Dashboard/SettingComp.tsx
@@ -1,5 +1,6 @@
 import { useState, useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import ResetPasswordPopup from "./PasswordPopup";
 
 const SettingComp: React.FC = () => {
   const authContext = useContext(AuthContext);
@@ -28,6 +29,8 @@ const SettingComp: React.FC = () => {
     console.log("Updating user with:", formData);
     await updateUser(formData);
   };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <main className="flex flex-col items-center bg-grey-50 border b-black rounded-lg w-full">
@@ -60,10 +63,19 @@ const SettingComp: React.FC = () => {
       >
         Update Profile
       </button>
-
-      <button className="self-center mt-5 cursor-pointer w-[40%] px-3 py-2 bg-light-red border border-black text-black rounded-lg hover:bg-red/50 transition-colors duration-300">
+        
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className="self-center mt-5 cursor-pointer w-[40%] px-3 py-2 bg-light-red border border-black text-black rounded-lg hover:bg-red/50 transition-colors duration-300"
+      >
         Reset Password
       </button>
+
+      {/* Show Reset Password Modal when triggered */}
+      {isModalOpen && (
+        <ResetPasswordPopup username={user.username} onClose={() => setIsModalOpen(false)} />
+      )}
+
     </main>
   );
 };
