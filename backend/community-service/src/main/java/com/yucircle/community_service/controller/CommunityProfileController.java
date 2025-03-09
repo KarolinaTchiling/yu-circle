92cb12030e65b4b6c231480e9ea463acd9ee7b05
CocoJumbo0 <118237316+CocoJumbo0@users.noreply.github.com>
2025-03-09 00:39:00 -0500

    Added /remove-profile-tag (#26)
    Modified a few comments and did some refactoring

--- backend/community-service/src/main/java/com/yucircle/community_service/controller/CommunityProfileController.java
+++ backend/community-service/src/main/java/com/yucircle/community_service/controller/CommunityProfileController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.yucircle.community_service.model.Profile;
 import com.yucircle.community_service.model.ProfileTag;
 import com.yucircle.community_service.model.ProfileTagsDTO;
 import com.yucircle.community_service.service.CommunityService;
+import com.yucircle.community_service.service.ProfileTaggingService;
 
 @RestController
 @RequestMapping("community")
@@ -25,11 +27,14 @@ public class CommunityProfileController {
 	@Autowired
 	private CommunityService commService;
 	
+	@Autowired
+	private ProfileTaggingService ptService;
+	
 	
 	//return list of tags belonging to given profile
 	@GetMapping("/get-profile-tags")
 	public List<String> getProfileTags(@RequestBody Profile profile) {
-		return commService.getProfileTags(profile.getUsername());
+		return ptService.getProfileTags(profile.getUsername());
 	}
 	
 	//add tag to profile
@@ -38,7 +43,20 @@ public class CommunityProfileController {
 	@PostMapping("/add-profile-tag") 
 	public ResponseEntity<HttpStatus> addProfileTag(@RequestBody ProfileTag pt) {
 		try {
-			commService.addProfileTag(pt.getProfile().getUsername(), pt.getTag().getTag());
+			ptService.addProfileTag(pt.getProfile().getUsername(), pt.getTag().getTag());
+			return ResponseEntity.ok(HttpStatus.OK);
+		}
+		catch (Exception e) {
+			return ResponseEntity.notFound().build();			
+		}
+	}
+	
+	//remove tag from profile
+	//RequestBody should have a JSON containing both profile and the tag to remove
+	@DeleteMapping("/remove-profile-tag")
+	public ResponseEntity<HttpStatus> removeProfileTag(@RequestBody ProfileTag pt) {
+		try {
+			ptService.removeProfileTag(pt.getProfile().getUsername(), pt.getTag().getTag());
 			return ResponseEntity.ok(HttpStatus.OK);
 		}
 		catch (Exception e) {
@@ -61,14 +79,14 @@ public class CommunityProfileController {
 	}
 	
 	//Filter and return profiles with specified tag
-	//URL Example: localhost:8081/community/filter?tag=Chemistry
+	//URL Example: localhost:8080/community/filter?tag=Chemistry
 	@GetMapping("/filter")
 	public List<ProfileTagsDTO> filterTags(@RequestParam String tag) {
 		return commService.filterTags(tag);
 	}
 	
 	//Return potential matches to search query
-	//URL Example: localhost:8081/community/search?query=j
+	//URL Example: localhost:8080/community/search?query=j
 	@GetMapping("/search")
 	public List<ProfileTagsDTO> searchUser(@RequestParam String query) {
 		return commService.queryProfile(query);
--- backend/community-service/src/main/java/com/yucircle/community_service/repositories/ProfileTagRepository.java
+++ backend/community-service/src/main/java/com/yucircle/community_service/repositories/ProfileTagRepository.java
@@ -2,6 +2,10 @@ package com.yucircle.community_service.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.yucircle.community_service.model.Profile;
 import com.yucircle.community_service.model.ProfileTag;
+import com.yucircle.community_service.model.Tag;
 
-public interface ProfileTagRepository extends JpaRepository<ProfileTag, Long> {}
+public interface ProfileTagRepository extends JpaRepository<ProfileTag, Long> {
+	void deleteByProfileAndTag(Profile profile, Tag tag);
+}
