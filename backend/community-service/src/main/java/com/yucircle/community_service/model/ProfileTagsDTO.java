576fcc0f74d6d90bc5586d70f3dc24ad289b82f5
CocoJumbo0 <118237316+CocoJumbo0@users.noreply.github.com>
2025-03-12 00:33:52 -0400

    Community service (#27)
    * Added sorting and fixed duplicate profile bug
    
    Can now sort by alphabet and reverse alphabet

--- backend/community-service/src/main/java/com/yucircle/community_service/model/ProfileTagsDTO.java
+++ backend/community-service/src/main/java/com/yucircle/community_service/model/ProfileTagsDTO.java
@@ -1,6 +1,7 @@
 package com.yucircle.community_service.model;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class ProfileTagsDTO {
@@ -110,4 +111,21 @@ public class ProfileTagsDTO {
     		this.tags.add(t.getTag());
     	}
     }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProfileTagsDTO other = (ProfileTagsDTO) obj;
+		return Objects.equals(username, other.username);
+	}    
 }
