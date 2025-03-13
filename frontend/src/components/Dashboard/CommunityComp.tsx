1d8ba6578a17363bfee1fea36ccd00653d96d466
Yasmine <91900711+yasmine-thandi@users.noreply.github.com>
2025-03-13 13:32:15 -0400

    Update CommunityComp.tsx
    Commenting code for testing.

--- frontend/src/components/Dashboard/CommunityComp.tsx
+++ frontend/src/components/Dashboard/CommunityComp.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { CheckCircle, UserCircle } from "lucide-react";
@@ -59,6 +59,6 @@ const CommunityComp: React.FC<{ filter?: string }> = ({ filter }) => {
         ))}
     </div>
   );
-};
+};*/
 
 export default CommunityComp;
