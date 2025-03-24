138dacbee9fe4ef3190fb89fcf7140aa2a5463f8
nramnara <99999037+nramnara@users.noreply.github.com>
2025-03-24 02:23:45 -0400

    Added delete, getbysender, getbyreceiver.

---
+++ backend/YUCircleMessagingService/src/main/java/com/yucircle/messagingapp/YuCircleMessagingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yucircle.messagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuCircleMessagingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuCircleMessagingServiceApplication.class, args);
+	}
+
+}
