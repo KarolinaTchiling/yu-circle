87b9e4441abc395f8e30f2ebe0672e53798480c7
nramnara <99999037+nramnara@users.noreply.github.com>
2025-03-27 22:46:27 -0400

    Marketplace upload

---
+++ backend/YUCircleMarketplaceService/src/main/java/com/yucircle/marketplaceapp/YuCircleMarketplaceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yucircle.marketplaceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuCircleMarketplaceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuCircleMarketplaceServiceApplication.class, args);
+	}
+
+}
