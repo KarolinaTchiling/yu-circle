7552dbc56df55297120dbdf66d4799c937980380
nramnara <99999037+nramnara@users.noreply.github.com>
2025-03-27 23:58:47 -0400

    Delete backend/YUCircleDiscourseService directory

--- backend/YUCircleDiscourseService/src/main/java/com/yucircle/discourceapp/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.yucircle.discourceapp.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
