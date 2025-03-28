5a9e6b5f4f645c2a696a06c50319ca36c645b2aa
Karolina Tchiling <kdt@my.yorku.ca>
2025-03-28 10:26:05 -0400

    add endpoint to marketplace get-all products by username

--- backend/YUCircleMarketplaceService/src/main/java/com/yucircle/marketplaceapp/repository/ProductRepository.java
+++ backend/YUCircleMarketplaceService/src/main/java/com/yucircle/marketplaceapp/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.yucircle.marketplaceapp.repository;
 
 import java.util.Optional;
+import java.util.List;
+
 
 //import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.yucircle.marketplaceapp.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByUsername(String username);
 }
