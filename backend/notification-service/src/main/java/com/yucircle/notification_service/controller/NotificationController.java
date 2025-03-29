419170ab7f21976d93b6186dfcc76d595cdab164
Sophia L <118237316+CocoJumbo0@users.noreply.github.com>
2025-03-28 22:09:35 -0400

    Added notification endpoint to handle deletion

--- backend/notification-service/src/main/java/com/yucircle/notification_service/controller/NotificationController.java
+++ backend/notification-service/src/main/java/com/yucircle/notification_service/controller/NotificationController.java
@@ -1,9 +1,11 @@
 package com.yucircle.notification_service.controller;
 
 import java.util.Deque;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,4 +26,13 @@ public class NotificationController {
 		
 		return nService.getAllUserNotifications(username);
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<HttpStatus> removeNotification(@PathVariable Long id) {
+		
+		nService.removeNotification(id);
+
+		return ResponseEntity.ok(HttpStatus.OK);
+	}
+	
 }
